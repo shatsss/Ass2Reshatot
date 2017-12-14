96aff370abd02ee8cbb3dad436257c0b6a916a92
אלון שץ <alonshats49@gmail.com>
2017-12-14 11:42:47 +0200

    bb

--- tcp_server.py
+++ tcp_server.py
@@ -45,21 +45,23 @@ def get_date():
 def makeStatic(req, type):
     req = DeleteParams(req)
     if type is None or req is None:
-        res = CreateResponse('HTTP/1.1', '404 Not Found', 'close', 'text', 'html', 'Not Found')
+        res = CreateResponse('HTTP/1.1', '404 Not Found', 'close', 'text', 'html', 'Not Found', True)
     else:
         if type == 'js':
             type = "javascript"
         try:
             file = open(req, "rb")
             content = file.read()
-            res = CreateResponse('HTTP/1.1', '200 OK', 'close', mappings.getMappingsDictionary()[type], type, content)
+            res = CreateResponse('HTTP/1.1', '200 OK', 'close', mappings.getMappingsDictionary()[type],
+                                 type, content, True)
         except:
             try:
                 file = open("Files/" + req, "rb")
                 content = file.read()
-                res = CreateResponse('HTTP/1.1', '200 OK', 'close', mappings.getMappingsDictionary()[type], type, content)
+                res = CreateResponse('HTTP/1.1', '200 OK', 'close', mappings.getMappingsDictionary()[type], type,
+                                     content, True)
             except:
-                res = CreateResponse('HTTP/1.1', '404 Not Found', 'close', 'text', 'html', 'Not Found')
+                res = CreateResponse('HTTP/1.1', '404 Not Found', 'close', 'text', 'html', 'Not Found', True)
     return res
 
 
@@ -97,12 +99,13 @@ def MakeDynamicPage(file, numberOfArticles):
     res = ''
     for i in range(numberOfArticles):
         resource = db.GetList(i)
-        res += before_section_Row_End.replace('Title', resource.title).replace('Content', resource.content).replace(
-            'link', resource.link).replace('src=""', 'src="' + resource.picture + '"')
+        res += section_Row_Begin + before_section_Row_End.replace('Title', resource.title).replace('Content',
+                                                                                                   resource.content).replace(
+            'link', resource.link).replace('src=""', 'src="' + resource.picture + '"') + section_Row_End
 
     # create all the html file
     res = before_section_Id_Start + section_Id_Start + before_section_Row_Begin + \
-          section_Row_Begin + res + section_Row_End + after_section_Row_End + section_Id_End + after_section_Id_End
+          res + after_section_Row_End + section_Id_End + after_section_Id_End
     return res
 
 
@@ -110,12 +113,12 @@ def makeDynamic(numberOfArticles):
     try:
         file = open('Files/template.html', "rb").read()
         if numberOfArticles == 0:
-            res = CreateResponse('HTTP/1.1', '200 OK', 'close', 'text', 'html', file)
+            res = CreateResponse('HTTP/1.1', '200 OK', 'close', 'text', 'html', file, True)
         else:
             data = MakeDynamicPage(file, numberOfArticles)
-            res = CreateResponse('HTTP/1.1', '200 OK', 'close', 'text', 'html', data)
+            res = CreateResponse('HTTP/1.1', '200 OK', 'close', 'text', 'html', data, False)
     except:
-        res = CreateResponse('HTTP/1.1', '404 Not Found', 'close', 'text', 'html', 'Not Found')
+        res = CreateResponse('HTTP/1.1', '404 Not Found', 'close', 'text', 'html', 'Not Found', True)
     return res
 
 
@@ -127,32 +130,41 @@ def SendReq(req, type):
         return makeStatic(req, type)
 
 
+def Create304Response():
+    return CreateResponse('HTTP/1.1', '304 Not Modified', 'close', 'text', 'html', 'Not Modified', True)
+
+
 def MakeResponse(data):
+    if 'If-Modified-Since' in data:
+        return Create304Response()
     req = GetRequestFromClient(data)
     type = None
     if req is not None:
         type = CheckTypeOfRequest(req)
-    print('Received: ', data)
     return SendReq(req, type)
 
 
-def CreateResponse(version, resNum, connection, type1, type2, data):
+def CreateResponse(version, resNum, connection, type1, type2, data, modifiedSinceFlag):
     res = version + ' ' + resNum + '\n' + 'Date: ' + get_date() + ' GMT\n' + 'Content-Length: ' + \
-          str(len(data)) + '\n' + 'Connection: ' + connection + '\n' + \
-          'Content-Type: ' + type1 + '/' + type2 + '\n\n' + data
+          str(len(data)) + '\n' + 'Connection: ' + connection + '\n'
+    if modifiedSinceFlag is True:
+        res += 'Last-Modified: ' + startDate + 'GMT\n'
+    res += 'Content-Type: ' + type1 + '/' + type2 + '\n\n' + data
     return res
 
 
 # the main of the program, DNS server- return the answer to the client
 def main():
-    global db
+    global db, startDate, mappings
+    startDate = get_date()
     db = DB()
-    global mappings
     mappings = Mappings()
     # create dict of types
     while True:
         client_socket, client_address = server.accept()
+        print "Client Connect: ", client_address
         data = client_socket.recv(1024)
+        print data
         while not data == '':
             res = MakeResponse(data)
             client_socket.send(res)
